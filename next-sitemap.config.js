569a0c3666aa727647ffcdab34643aec32640e84
AhmedYounsi <hammoua07@gmail.com>
2022-06-29 05:11:34 +0100

    dg

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: process.env.SITE_URL || 'https://tunisie-dentiste.tn',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
+  export default config
